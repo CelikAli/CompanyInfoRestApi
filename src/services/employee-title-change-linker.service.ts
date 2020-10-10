d71be414e3c15ac3ee346aecd4d0f94afb0c125e
Ali Celik <aliemrecelik17@gmail.com>
2020-10-10 18:54:02 +0300

    Add the functionality of creating a new TitleChange when creating a new Employee

--- src/services/employee-title-change-linker.service.ts
+++ src/services/employee-title-change-linker.service.ts
@@ -1,9 +1,23 @@
 import {/* inject, */ BindingScope, injectable} from '@loopback/core';
-import {Employee} from '../models';
+import {repository} from '@loopback/repository';
+import {Employee, TitleChange} from '../models';
+import {TitleChangeRepository} from '../repositories';
 
 @injectable({scope: BindingScope.TRANSIENT})
 export class EmployeeTitleChangeLinkerService {
-  constructor(/* Add @inject to inject parameters */) {}
+  constructor(
+    @repository(TitleChangeRepository)
+    public titleChangeRepository: TitleChangeRepository,
+  ) {}
 
-  createNewTitleChange(employee: Employee) {}
+  async createNewTitleChange(employee: Employee): Promise<TitleChange> {
+    const titleChange: Omit<TitleChange, 'id'> = new TitleChange({
+      title: employee.title,
+      startDate: employee.employmentDate,
+      departmentId: employee.departmentId,
+      employeeId: employee.id,
+    });
+
+    return this.titleChangeRepository.create(titleChange);
+  }
 }
